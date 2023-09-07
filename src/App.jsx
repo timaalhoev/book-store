0aa04f306a636b398b4e11f99d2bc2ff98201734
Timur Alhoev <timuralhoev@yandex.ru>
2023-09-07 21:43:30 +0300

    первый пуш верстка и запрос fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import { booksApiUrl } from "./constants";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  useEffect(() => {
+    (async () => {
+      console.log(booksApiUrl + `&key=${import.meta.env.VITE_API_KEY}`);
+      const res = await fetch(
+        booksApiUrl + `&key=${import.meta.env.VITE_API_KEY}`
+      );
+      console.log(res);
+    })();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="wrapper">
+      <header>
+        <h1>Search for books</h1>
+        <div className="search">
+          <input className="search__field" type="text"></input>
+          <button className="search__button">Search</button>
+        </div>
+        {/* <img className="logo" src="../src/assets/images/fon.png" /> */}
+        <div className="sort">
+          <div className="sortField">
+            <label>Categories</label>
+            <input></input>
+          </div>
+          <div className="sortField">
+            <label>Sorting by</label>
+            <input></input>
+          </div>
+        </div>
+      </header>
+      <div className="content">
+        <p>Found 446 results</p>
+        <div className="card__row">
+          <div className="card">
+            <img />
+
+            <div className="card__block-info">
+              <p>Computers</p>
+              <h4>Node.js. Путеводитель по технологиям</h4>
+              <p>Кирилл Сухов</p>
+            </div>
+          </div>
+          <div className="card">
+            <img />
+
+            <div className="card__block-info">
+              <p>Computers</p>
+              <h4>Node.js. Путеводитель по технологиям</h4>
+              <p>Кирилл Сухов</p>
+            </div>
+          </div>
+          <div className="card">
+            <img />
+
+            <div className="card__block-info">
+              <p>Computers</p>
+              <h4>Node.js. Путеводитель по технологиям</h4>
+              <p>Кирилл Сухов</p>
+            </div>
+          </div>
+          <div className="card">
+            <img />
+
+            <div className="card__block-info">
+              <p>Computers</p>
+              <h4>Node.js. Путеводитель по технологиям</h4>
+              <p>Кирилл Сухов</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const booksApiUrl = "https://www.googleapis.com/books/v1/volumes?q=dogs";
